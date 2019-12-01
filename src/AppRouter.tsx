f971c95695424b7896b94c8e0e0c5e4af20d1383
andymk <andym.knight@gmail.com>
2019-12-01 19:49:40 +0000

    Accounts page added

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,19 +1,19 @@
 import { ConnectedRouter } from "connected-react-router";
 import { Route, Switch, Redirect } from "react-router-dom";
-import HomePage from "./pages/home";
+import DashboardPage from "./pages/dashboard-page";
 import LoginPage from "./pages/login/loginpage";
 import { appHistory } from "./AppHistory";
 import React from "react";
-import BudgetPage from "./pages/budget-page";
+import AccountsPage from "./pages/accounts-page";
 import TransactionsPage from "./pages/transactions";
 
 export const AppRouter = () => {
   return (
     <ConnectedRouter history={appHistory()}>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={DashboardPage} />
         <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/budgets" component={BudgetPage} />
+        <Route exact path="/accounts" component={AccountsPage} />
         <Route
           path="/transactions/accounts/:accountid"
           component={TransactionsPage}
--- src/components/pageheader.tsx
+++ src/components/pageheader.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-const PageHeader = () => {
+const PageHeader = (props: any) => {
   return (
     <section className="content-header">
       <div className="container-fluid">
         <div className="row mb-2">
           <div className="col-sm-6">
-            <h1>Dashboard</h1>
+            <h1>{props.title}</h1>
           </div>
           <div className="col-sm-6">
             <ol className="breadcrumb float-sm-right">
               <li className="breadcrumb-item">
                 <a href="#">Home</a>
               </li>
-              <li className="breadcrumb-item active">Dashboard</li>
+              <li className="breadcrumb-item active">{props.title}</li>
             </ol>
           </div>
         </div>
